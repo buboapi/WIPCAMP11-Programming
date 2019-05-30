bd3920ac21b0dcd5a9d3b0c0a9eb33faca759481
buboapi <abbowoo_@users.noreply.github.com>
2019-05-30 11:47:04 +0700

    [ADD]create project

---
+++ WIP-XI/src/V/Selection/whatttttt.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2019 wipcamp11.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+package V.Selection;
+
+/**
+ *
+ * @author wipcamp11
+ */
+import java.util.Scanner;
+public class whatttttt {
+
+    public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+
+int x = sc.nextInt();
+        System.out.println("มีบัตรสมาชิกมั้ย?");
+boolean membercard =true;
+    if(membercard){
+            System.out.println(x*95/100);                   
+    }
+
+        System.out.println("จ่ายด้วยเงินสดมั้ย?");
+        
+        boolean cash=true;
+        
+        if(cash){
+            System.out.println(x*90/100);                   
+    }
+  
+    
+            
+}
+    }
